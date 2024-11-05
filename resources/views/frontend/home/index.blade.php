91a528ca819c8a45816cf498d59926fd7f85ab09
vchandra22 <vincentchaandra@gmail.com>
2024-11-05 20:40:54 +0700

    feat: create frontend section berita with cards

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -142,26 +142,26 @@
                         <div class="flex flex-col gap-4 p-4">
                             <h3 class="text-4xl text-slate-800 font-semibold">Sambutan Kepala Sekolah</h3>
                             <p class="text-md text-justify overflow-hidden font-light tracking-tight line-clamp-6 text-slate-400 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cum debitis doloremque eveniet explicabo illo modi provident repellendus? Aut blanditiis deleniti error odit officia reprehenderit. Accusantium aliquam animi at atque consequatur delectus dignissimos distinctio doloribus ducimus eius esse expedita fugit inventore itaque labore laborum libero modi, molestias omnis pariatur perferendis porro quia quidem ratione sequi soluta ut. A ad, adipisci aspernatur assumenda beatae culpa cumque cupiditate delectus dicta dolor dolorem doloribus ducimus earum eius facilis labore maxime modi, nesciunt, nihil nulla officia omnis quasi recusandae suscipit veniam vero voluptate voluptates. Odio, quod?</p>
-                            <a href="#" class="inline-flex items-start px-0 py-2 text-md font-normal text-center md:text-start text-red-500 hover:underline hover:text-red-800">
+                            <a href="#" class="inline-flex items-start px-0 py-2 text-md font-normal text-center md:text-start text-red-600 hover:underline hover:text-red-500">
                                 Selengkapnya...
                             </a>
                         </div>
                     </div>
                     <div class="col-span-2 w-full h-full">
                         <div class="flex flex-col items-center gap-8 md:gap-10">
-                            <h3 class="text-4xl text-red-500 text-center font-semibold">Statistik Data Sekolah</h3>
+                            <h3 class="text-4xl text-red-600 text-center font-semibold">Statistik Data Sekolah</h3>
                             <div class="flex flex-col md:flex-row gap-4 items-center w-full justify-center">
                                 <div class="w-full h-full text-center">
-                                    <h5 class="text-7xl md:text-7xl xl:text-8xl font-bold text-red-500">32</h5>
-                                    <h5 class="text-xl lg:text-2xl mt-4 md:mt-8 font-semibold text-red-500 min-h-20">Guru & Staff</h5>
+                                    <h5 class="text-7xl md:text-7xl xl:text-8xl font-bold text-red-600">32</h5>
+                                    <h5 class="text-xl lg:text-2xl mt-4 md:mt-8 font-semibold text-red-600 min-h-20">Guru & Staff</h5>
                                 </div>
                                     <div class="w-full h-full text-center">
-                                        <h5 class="text-7xl md:text-7xl xl:text-8xl font-bold text-red-500">248</h5>
-                                        <h5 class="text-xl lg:text-2xl mt-4 md:mt-8 font-semibold text-red-500 min-h-20">Peserta Didik</h5>
+                                        <h5 class="text-7xl md:text-7xl xl:text-8xl font-bold text-red-600">248</h5>
+                                        <h5 class="text-xl lg:text-2xl mt-4 md:mt-8 font-semibold text-red-600 min-h-20">Peserta Didik</h5>
                                     </div>
                                     <div class="w-full h-full text-center">
-                                        <h5 class="text-7xl md:text-7xl xl:text-8xl font-bold text-red-500">12</h5>
-                                        <h5 class="text-xl lg:text-2xl mt-4 md:mt-8 font-semibold text-red-500 min-h-20">Rombel</h5>
+                                        <h5 class="text-7xl md:text-7xl xl:text-8xl font-bold text-red-600">12</h5>
+                                        <h5 class="text-xl lg:text-2xl mt-4 md:mt-8 font-semibold text-red-600 min-h-20">Rombel</h5>
                                     </div>
                             </div>
                         </div>
@@ -179,7 +179,7 @@
                         <h2 class="text-red-600 text-4xl md:text-6xl font-extrabold text-start">Agenda Kegiatan</h2>
                         <p class="mt-4 text-md font-normal tracking-wide text-slate-800">Agenda kegiatan dan aktivitas sesuai dengan kalender pendidikan</p>
                     </div>
-                    <a href="#" class="inline-flex items-center justify-start p-4 rounded-full md:px-8 md:py-2 text-sm font-normal text-center text-slate-50 bg-slate-800 rounded-md hover:bg-slate-700">
+                    <a href="#" class="inline-flex items-center justify-start p-4 md:px-8 md:py-2 text-sm font-normal text-center text-slate-50 bg-slate-800 rounded-md hover:bg-slate-700">
                         <span class="hidden md:block">
                             Selengkapnya
                         </span>
@@ -189,7 +189,7 @@
                     </a>
                 </div>
                 <div class="mt-12 grid grid-cols-1 md:grid-cols-3 gap-4 justify-center items-center w-full">
-                    <div class="flex justify-start items-center gap-8 block w-full px-6 py-4 bg-white border border-slate-200 rounded-md hover:bg-slate-50">
+                    <div class="flex justify-start items-center gap-8 w-full px-6 py-4 bg-white border border-slate-200 rounded-md hover:bg-slate-50">
                         <div class="text-slate-800">
                             <i class="fa-solid fa-calendar fa-4x"></i>
                         </div>
@@ -202,7 +202,7 @@
                         </div>
                     </div>
 
-                    <div class="flex justify-start items-center gap-8 block w-full px-6 py-4 bg-white border border-slate-200 rounded-md hover:bg-slate-50">
+                    <div class="flex justify-start items-center gap-8 w-full px-6 py-4 bg-white border border-slate-200 rounded-md hover:bg-slate-50">
                         <div class="text-slate-800">
                             <i class="fa-solid fa-calendar fa-4x"></i>
                         </div>
@@ -215,7 +215,7 @@
                         </div>
                     </div>
 
-                    <div class="flex justify-start items-center gap-8 block w-full px-6 py-4 bg-white border border-slate-200 rounded-md hover:bg-slate-50">
+                    <div class="flex justify-start items-center gap-8 w-full px-6 py-4 bg-white border border-slate-200 rounded-md hover:bg-slate-50">
                         <div class="text-slate-800">
                             <i class="fa-solid fa-calendar fa-4x"></i>
                         </div>
@@ -231,9 +231,129 @@
             </div>
         </div>
         {{--agenda kegiatan end--}}
-        <div>
 
+        {{--berita artikel informasi start--}}
+        <div class="bg-slate-50 py-20 px-4">
+            <div class="max-w-screen-2xl mx-auto">
+                <div>
+                    <h2 class="text-red-600 text-4xl md:text-6xl font-extrabold text-center">Berita, Artikel & Informasi</h2>
+                    <p class="mt-4 text-md font-normal text-center tracking-wide text-slate-800">Simak laporan langsung dari berbagai kegiatan sekolah.</p>
+                </div>
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
+                    <a href="#" class="relative w-full h-64 lg:h-72 border-2 bg-slate-100 border-slate-400 rounded-lg group" data-aos="fade"
+                         data-aos-delay="600" data-aos-duration="600" data-aos-easing="ease-in-out" data-aos-once="true">
+                        <img src="{{ asset("assets/img/carousel-image.webp") }}"
+                             class="h-full w-full overflow-hidden object-cover mx-auto rounded-lg" width="100"
+                             height="100" alt="#">
+                        <div
+                            class="absolute top-0 right-0 h-full w-full bg-slate-800 bg-opacity-60 group-hover:bg-opacity-90 ease-in-out">
+                        </div>
+                        <div class="absolute top-0 right-0 h-full w-full px-6 py-6 md:w-full">
+                            <div class="h-full flex flex-col justify-end">
+                                <h3
+                                    class="text-slate-50 font-semibold min-h-20 text-start text-2xl md:text-4xl overflow-hidden line-clamp-2">
+                                    Mengasah Bakat Seni Workshop Lukisan dan Seni Kerajinan Lorem ipsum dolor sit.
+                                </h3>
+                                <p
+                                    class="mt-2 text-slate-50 font-regular text-start text-md md:text-lg lg:text-xl tracking-tight overflow-hidden line-clamp-1">
+                                    10 Juli 2024 oleh Admin
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="relative w-full h-64 lg:h-72 border-2 bg-slate-100 border-slate-400 rounded-lg group" data-aos="fade"
+                       data-aos-delay="600" data-aos-duration="600" data-aos-easing="ease-in-out" data-aos-once="true">
+                        <img src="{{ asset("assets/img/carousel-image.webp") }}"
+                             class="h-full w-full overflow-hidden object-cover mx-auto rounded-lg" width="100"
+                             height="100" alt="#">
+                        <div
+                            class="absolute top-0 right-0 h-full w-full bg-slate-800 bg-opacity-60 group-hover:bg-opacity-90 ease-in-out">
+                        </div>
+                        <div class="absolute top-0 right-0 h-full w-full px-6 py-6 md:w-full">
+                            <div class="h-full flex flex-col justify-end">
+                                <h3
+                                    class="text-slate-50 font-semibold min-h-20 text-start text-2xl md:text-4xl overflow-hidden line-clamp-2">
+                                    Mengasah Bakat Seni Workshop Lukisan dan Seni Kerajinan Lorem ipsum dolor sit.
+                                </h3>
+                                <p
+                                    class="mt-2 text-slate-50 font-regular text-start text-md md:text-lg lg:text-xl tracking-tight overflow-hidden line-clamp-1">
+                                    10 Juli 2024 oleh Admin
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="relative w-full h-64 lg:h-72 border-2 bg-slate-100 border-slate-400 rounded-lg group" data-aos="fade"
+                       data-aos-delay="600" data-aos-duration="600" data-aos-easing="ease-in-out" data-aos-once="true">
+                        <img src="{{ asset("assets/img/carousel-image.webp") }}"
+                             class="h-full w-full overflow-hidden object-cover mx-auto rounded-lg" width="100"
+                             height="100" alt="#">
+                        <div
+                            class="absolute top-0 right-0 h-full w-full bg-slate-800 bg-opacity-60 group-hover:bg-opacity-90 ease-in-out">
+                        </div>
+                        <div class="absolute top-0 right-0 h-full w-full px-6 py-6 md:w-full">
+                            <div class="h-full flex flex-col justify-end">
+                                <h3
+                                    class="text-slate-50 font-semibold min-h-20 text-start text-2xl md:text-4xl overflow-hidden line-clamp-2">
+                                    Mengasah Bakat Seni Workshop Lukisan dan Seni Kerajinan Lorem ipsum dolor sit.
+                                </h3>
+                                <p
+                                    class="mt-2 text-slate-50 font-regular text-start text-md md:text-lg lg:text-xl tracking-tight overflow-hidden line-clamp-1">
+                                    10 Juli 2024 oleh Admin
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="relative w-full h-64 lg:h-72 border-2 bg-slate-100 border-slate-400 rounded-lg group" data-aos="fade"
+                       data-aos-delay="600" data-aos-duration="600" data-aos-easing="ease-in-out" data-aos-once="true">
+                        <img src="{{ asset("assets/img/carousel-image.webp") }}"
+                             class="h-full w-full overflow-hidden object-cover mx-auto rounded-lg" width="100"
+                             height="100" alt="#">
+                        <div
+                            class="absolute top-0 right-0 h-full w-full bg-slate-800 bg-opacity-60 group-hover:bg-opacity-90 ease-in-out">
+                        </div>
+                        <div class="absolute top-0 right-0 h-full w-full px-6 py-6 md:w-full">
+                            <div class="h-full flex flex-col justify-end">
+                                <h3
+                                    class="text-slate-50 font-semibold min-h-20 text-start text-2xl md:text-4xl overflow-hidden line-clamp-2">
+                                    Mengasah Bakat Seni Workshop Lukisan dan Seni Kerajinan Lorem ipsum dolor sit.
+                                </h3>
+                                <p
+                                    class="mt-2 text-slate-50 font-regular text-start text-md md:text-lg lg:text-xl tracking-tight overflow-hidden line-clamp-1">
+                                    10 Juli 2024 oleh Admin
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="relative w-full h-64 lg:h-72 border-2 bg-slate-100 border-slate-400 rounded-lg group" data-aos="fade"
+                       data-aos-delay="600" data-aos-duration="600" data-aos-easing="ease-in-out" data-aos-once="true">
+                        <img src="{{ asset("assets/img/carousel-image.webp") }}"
+                             class="h-full w-full overflow-hidden object-cover mx-auto rounded-lg" width="100"
+                             height="100" alt="#">
+                        <div
+                            class="absolute top-0 right-0 h-full w-full bg-slate-800 bg-opacity-60 group-hover:bg-opacity-90 ease-in-out">
+                        </div>
+                        <div class="absolute top-0 right-0 h-full w-full px-6 py-6 md:w-full">
+                            <div class="h-full flex flex-col justify-end">
+                                <h3
+                                    class="text-slate-50 font-semibold min-h-20 text-start text-2xl md:text-4xl overflow-hidden line-clamp-2">
+                                    Mengasah Bakat Seni Workshop Lukisan dan Seni Kerajinan Lorem ipsum dolor sit.
+                                </h3>
+                                <p
+                                    class="mt-2 text-slate-50 font-regular text-start text-md md:text-lg lg:text-xl tracking-tight overflow-hidden line-clamp-1">
+                                    10 Juli 2024 oleh Admin
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
         </div>
+        {{--berita artikel informasi end--}}
 
     </section>
 @endsection
